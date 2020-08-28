1da8d198e001bf6e4fdfb424844c835b5fc6518e
GrapeColor <grapecolor@users.noreply.github.com>
2020-08-28 20:29:41 +0900

    Change error generation process

--- src/quick_poll/error.js
+++ src/quick_poll/error.js
@@ -1,14 +1,25 @@
 'use strict';
 
 const { locales } = require('./locales');
-const errors = locales.ja.poll.error;
 
 class PollError {
-  constructor(exception) {
-    const error = errors[exception];
-    this.title = error.title;
-    this.description = error.description;
+  constructor(exception, lang = 'ja', variables = {}) {
+    const error = locales[lang].errors.poll[exception];
+    const infomation = locales[lang].errors.infomation;
+
+    let title = error.title;
+    let description = `${error.description ?? ''}\n\n${infomation}`;
+
+    variables = { ...variables, ...process.env };
+
+    title = title.replace(/\$\{(\w+)\}/g, (_, key) => this.replacer(variables, key));
+    description = description.replace(/\$\{(\w+)\}/g, (_, key) => this.replacer(variables, key));
+
+    this.title = title;
+    this.description = description;
   }
+
+  replacer(variables, key) { return `${variables[key] ?? '' }`; }
 }
 
 module.exports = PollError;
