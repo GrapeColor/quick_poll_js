e1c6fc6549db567557ec9247827c5fe7b2adba16
GrapeColor <grapecolor@users.noreply.github.com>
2020-09-04 01:51:25 +0900

    Chang to resolve variables in locale loader

--- src/quick_poll/locales.js
+++ src/quick_poll/locales.js
@@ -3,4 +3,19 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
 
-exports.locales = yaml.safeLoad(fs.readFileSync(`${__dirname}/locales/locales.yml`, 'utf-8'));
+const constants = require('./constants');
+const variables = { ...process.env, ...constants };
+
+const replacer = key => { return variables[key] ?? ''; }
+
+const localesData = () => {
+  let data = fs.readFileSync(`${__dirname}/locales/locales.yml`, 'utf-8');
+
+  data = data.replace(/\$\{(\w+)\}/g, (_, key) => replacer(key));
+
+  return yaml.safeLoad(data);
+}
+
+console.log(localesData());
+
+exports.locales = localesData();
