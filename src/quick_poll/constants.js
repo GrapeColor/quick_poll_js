c1a61aac5c686dd0ad176441ee20a70f48379675
GrapeColor <grapecolor@users.noreply.github.com>
2020-09-04 01:53:05 +0900

    Add a poll-related constant

--- src/quick_poll/constants.js
+++ src/quick_poll/constants.js
@@ -7,6 +7,8 @@ exports.REQUIRED_PERMISSIONS = new Permissions(388160);
 exports.DEFAULT_PREFIX = '/';
 exports.DEFAULT_LOCALE = 'ja';
 
+exports.QUEUE_TIMEOUT = 60000;
+
 exports.COLOR_POLL   = 0x3b88c3;
 exports.COLOR_EXPOLL = 0x3b88c4;
 exports.COLOR_RESULT = 0xdd2e44;
@@ -14,4 +16,6 @@ exports.COLOR_WAIT   = 0x9867c6;
 exports.COLOR_ERROR  = 0xffcc4d;
 exports.COLOR_HELP   = 0xff922f;
 
-exports.QUEUE_TIMEOUT = 60000;
+exports.MAX_OPTIONS = 20;
+exports.QUERY_MAX = 200;
+exports.OPTION_MAX = 200;
