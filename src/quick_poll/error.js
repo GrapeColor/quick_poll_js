9fedfa3b7614f7fe43f630bc1d90ada39917585b
GrapeColor <grapecolor@users.noreply.github.com>
2020-09-06 03:16:41 +0900

    Rename PollError to CommandError

--- src/quick_poll/error.js
+++ src/quick_poll/error.js
@@ -2,7 +2,7 @@
 
 const { locales } = require('./locales');
 
-module.exports = class PollError {
+module.exports = class CommandError {
   constructor(response, exception, lang) {
     const errors = locales[lang].errors;
     const error = typeof exception === 'string' && errors.poll[exception] || errors.unexpect;
